f9f6c2f3f61dc82d50d5d9bc7520cfa3dc3e4ba5
Milan Pal Singh <milanpalsingh89@gmail.com>
2015-03-24 15:17:38 +0530

    using the express framework
    empty the 'node_modules' folder and run the 'npm install' command .

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-var connect = require('connect');
+// var connect = require('connect');
 
-var app = connect()
-	.use(connect.static('public'))
-	.use(function (req, res) {
-		res.end("Couldn't find it.");
-	})
-	.listen(3001);
+// var app = connect()
+// 	.use(connect.static('public'))
+// 	.use(function (req, res) {
+// 		res.end("Couldn't find it.");
+// 	})
+// 	.listen(3001);
+
+var express = require('express');
+
+var app = express();
+app.use(express.static('public'));
+
+app.get('/index',function(req,res){
+	res.send('we are in the index file');
+});
+
+app.listen(3001);
